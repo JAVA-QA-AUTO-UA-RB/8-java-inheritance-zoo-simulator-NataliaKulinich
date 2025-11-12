5db4539887d85f0e6cfb1a1d1d4cf6b1de0bb176
NataliaKulinich <kulinichnn@gmail.com>
2025-11-12 02:55:47 +0200

    Add files via upload

--- src/main/java/com/zoo/species/Penguin.java
+++ src/main/java/com/zoo/species/Penguin.java
@@ -1,4 +1,24 @@
 package com.zoo.species;
 
-public class Penguin {
+import com.zoo.animals.Bird;
+
+public class Penguin extends Bird {
+
+    public void makeSound ()
+    {   super.makeSound();
+        System.out.println(" як Пінгвін ... Кря-кря");    }
+
+    public void swim ()
+    {   System.out.println("Я -Пінгвін і я не вмію літати, але чудово плаваю");
+        setEnergyLevel( getEnergyLevel()-17);
+        happinessLevel-=1;  }
+
+    @Override
+    public void fly()
+    {     swim();      }
+
+    public void displayInfo()
+    {   super.displayInfo();
+        System.out.println ("Вид тварини: Пінгвіни");     };
+
 }
